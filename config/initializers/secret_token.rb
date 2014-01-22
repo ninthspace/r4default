4d4edf7b585ae7a0f1fbf18985f86f954058f285
Chris Aves <chris@junctionbox.com>
2014-01-22 13:20:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R4default::Application.config.secret_key_base = '86d26e65469f041b7d46df764001ad030fa1e4cc00ee6e8d002a8a5c006a2311455a0a0c67d4a9c9fad3851dbeb4f5db858c84ddae82683ed216a2b34bf1ef7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R4default::Application.config.session_store :cookie_store, key: '_r4default_session'
